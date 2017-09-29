6368501a984bfe18f50add3577211f4c203c0ebf
browna38 <ajbrown000@gmail.com>
2017-09-29 15:58:58 +0000

    Update forward.py

--- forward.py
+++ forward.py
@@ -9,7 +9,7 @@ import serial
 ser = serial.Serial(
   
    port='/dev/serial0',
-   baudrate = 2400,
+   baudrate = 38400,
    parity=serial.PARITY_NONE,
    stopbits=serial.STOPBITS_ONE,
    bytesize=serial.EIGHTBITS,
